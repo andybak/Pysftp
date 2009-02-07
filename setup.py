e4519e2907438a58330de382d1de4fd4c2356bbb
dundeemt <dundeemt@54395b2e-f4d3-11dd-802b-177316e4ac06>
2009-02-07 16:03:20 +0000

    initial upload
    git-svn-id: http://pysftp.googlecode.com/svn/trunk@2 54395b2e-f4d3-11dd-802b-177316e4ac06

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+setup(
+    name = "pysftp",
+    version = "0.1.0",
+    #packages = find_packages(),
+    py_modules = ['pysftp'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['paramiko>=1.7.4'],
+
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Jeff hinrichs",
+    author_email = "jeffh@dundeemt.com",
+    description = "A friendly face on SFTP",
+    license = "BSD",
+    keywords = "sftp ssh ftp internet",
+    url = "http://code.google.com/p/pysftp/",   # project home page, if any
+    long_description = """a simple interface to sftp.  based on zeth's ssh.py""",
+    platforms=['any'],
+
+    # could also include long_description, download_url, classifiers, etc.
+)
