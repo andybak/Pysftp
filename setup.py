14b525383de3f012c0e7647dd1652b6db2ad9a7e
Andy Baker <andy@andybak.net>
2012-01-22 17:04:13 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "pysftp",
-    version = "0.2.2",
+    version = "0.3",
     #packages = find_packages(),
     py_modules = ['pysftp'],
 
